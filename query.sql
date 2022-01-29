5fe3fcadf25b0f80e779c910a8674a7fdcbd0fbc
jbg469 <72175659+jbg469@users.noreply.github.com>
2022-01-28 20:17:41 -0500

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,6 +1,11 @@
-SELECT product_name, list_price, discount_percent,
-ROUND(list_price*discount_percent/100, 2) AS 'discount_amount',
-list_price-'discount_amount' AS 'discount_price'
-FROM products
-ORDER BY discount_price DESC
-LIMIT 5
+SELECT 
+    product_name AS 'product_name',
+    list_price AS 'list_price',
+    discount_percent AS 'discoun_tpercent',
+    ROUND(list_price * (discount_percent / 100), 2) AS 'discount_amount',
+    ROUND(list_price - list_price * (discount_percent / 100),
+            2) AS 'discount_price'
+FROM
+    products
+ORDER BY (list_price - list_price * (discount_percent / 100)) DESC
+LIMIT 5;

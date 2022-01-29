7694c58fec05289836ae09e2fa4ab83510b293d7
jbg469 <72175659+jbg469@users.noreply.github.com>
2022-01-28 21:17:59 -0500

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,7 +1,4 @@
-SELECT 
-    order_id AS 'order_id',
-    order_date AS 'order_date',
-    ship_date AS 'ship_date'
+SELECT order_id, order_date, ship_date
 FROM orders
-WHERE ship_date IS NULL
+WHERE ship_date IS null
 ORDER BY order_id DESC

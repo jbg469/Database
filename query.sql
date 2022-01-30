37651c4540ab41ee797ba1244a6a12a8e1b02fc0
jbg469 <72175659+jbg469@users.noreply.github.com>
2022-01-29 21:44:22 -0500

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,5 +1,5 @@
-SELECT c.first_name,c.last_name, a.line1,a.city,a.state, a.zip_code
-FROM customers AS c
-INNER JOIN addresses AS a ON c.customer_id = a.customer_id
-WHERE c.email_address = 'allan.sherwood@yahoo.com'
-ORDER BY a.zip_code ASC
+SELECT first_name,last_name, line1,city,state,zip_code
+FROM customers
+INNER JOIN addresses ON customer_id = customer_id
+WHERE email_address = 'allan.sherwood@yahoo.com'
+ORDER BY zip_code ASC

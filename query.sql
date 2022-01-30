f27812724e87d401b4c48901540a57d2e92bc544
jbg469 <72175659+jbg469@users.noreply.github.com>
2022-01-29 22:15:12 -0500

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,5 +1,4 @@
-SELECT customers.first_name, customers.last_name, addresses.line1, addresses.city, addresses.state, addresses.zip_code
-FROM customers 
-INNER JOIN addresses ON customers.customers_id = addresses.customer_id 
-WHERE (customers.customer_id = addresses.customer_id) AND (customers.shipping_address_id = addresses.address_id)
-ORDER BY addresses.zip_code ASC
+SELECT c.first_name,c.last_name,a.line1,a.city,a.state,a.zip_code 
+FROM customers c 
+RIGHT JOIN addresses a ON c.first_name=a.first_name
+ORDER BY a.zip_code ASC

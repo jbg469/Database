6d6cf58957b7323036419ea4525897596b369197
jbg469 <72175659+jbg469@users.noreply.github.com>
2022-01-29 22:33:09 -0500

    Create query.sql

---
+++ query.sql
@@ -0,0 +1,8 @@
+SELECT c.last_name, c.first_name, o.order_date, p.product_name, oi.item_price, oi.discount_amount, oi.quantity
+FROM customers c INNER JOIN orders o ON 
+c.customer_id = o.customer_id
+INNER JOIN order_items oi ON 
+o.order_id = oi.order_id
+INNER JOIN products p ON 
+oi.product_id = p.product_id
+ORDER BY c.last_name , o.order_date , p.product_name

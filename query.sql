3ec75792975b2afe3a68d97ccd28b4b62ee71b21
jbg469 <72175659+jbg469@users.noreply.github.com>
2022-01-29 22:22:45 -0500

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,4 +1,4 @@
 SELECT c.first_name,c.last_name,a.line1,a.city,a.state,a.zip_code 
 FROM customers AS c JOIN addresses AS a ON 
-c.customer_id = a.customer_id
+c.customer_id = a.customer_id AND c.shipping_address_id = a.address_id
 ORDER BY a.zip_code ASC

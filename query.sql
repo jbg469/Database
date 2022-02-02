46e77b7516ce5db1c9c7bbf4cb1b5c289af87c5a
jbg469 <72175659+jbg469@users.noreply.github.com>
2022-02-02 15:26:12 -0500

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,6 +1,6 @@
 SELECT  c.email_address,
-COUNT(oi.order_id) AS count,
-SUM(oi.item_price - oi.discount_amount) * COUNT(oi.order_id) AS total_amount
+COUNT(o.order_id) AS order_count,
+SUM(item_price - discount_amount) * quantity) AS order_total
 FROM customers AS c 
 INNER JOIN orders AS o ON c.customer_id = o.customer_id
 INNER JOIN order_items AS oi ON oi.order_id = o.order_id

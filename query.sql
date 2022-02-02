107bd4a992b7577ccbff9b75afba2032f9aa25c2
jbg469 <72175659+jbg469@users.noreply.github.com>
2022-02-02 17:14:04 -0500

    Update query.sql

--- query.sql
+++ query.sql
@@ -4,6 +4,6 @@ FROM customers c
         ON c.customer_id = o.customer_id
     INNER JOIN order_items oi
         ON oi.order_id = o.order_id
-GROUP BY c.customer_id
+GROUP BY email_address
 HAVING COUNT(o.order_id) > 1
 ORDER BY order_count DESC

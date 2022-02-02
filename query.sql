56d545c839fd9d8e189f7ec21b76477c0407bc76
jbg469 <72175659+jbg469@users.noreply.github.com>
2022-02-02 15:10:16 -0500

    Update query.sql

--- query.sql
+++ query.sql
@@ -2,6 +2,6 @@ SELECT c.email_address,
 SUM(o.item_price * o.quantity) AS 'item_price_total',
 SUM(o.discount_amount * o.quantity) AS 'discount_amount_total'
 FROM customers AS c
-JOIN order_items AS o ON c.id = o.customer_id
+JOIN order_items AS o ON c.customer_id = o.customer_id
 GROUP BY email_address
 ORDER BY item_price_total

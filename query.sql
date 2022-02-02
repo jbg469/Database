35808502210e0207b8d33b469f454257d48d233a
jbg469 <72175659+jbg469@users.noreply.github.com>
2022-02-02 16:41:10 -0500

    Create query.sql

--- query.sql
+++ query.sql
@@ -2,8 +2,8 @@ SELECT email_address,
 COUNT(o.order_id) AS order_count,
 SUM((item_price - discount_amount) * quantity) AS order_total
 FROM customers c
-JOIN orders o ON c.customer_id = o.customer_id
-JOIN order_items oi ON o.order_id = oi.order_id
+INNER JOIN orders o ON c.customer_id = o.customer_id
+INNER JOIN order_items oi ON o.order_id = oi.order_id
 GROUP by c.customer_id
 HAVING COUNT(o.order_id) > 1
-ORDER BY SUM(item_price) DESC
+ORDER BY order_count DESC

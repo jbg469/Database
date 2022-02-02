cb9efbfb0edd146da2ac6bd078971b31e28d3bbb
jbg469 <72175659+jbg469@users.noreply.github.com>
2022-02-02 15:29:53 -0500

    Update query.sql

--- query.sql
+++ query.sql
@@ -5,5 +5,5 @@ FROM customers AS c
 INNER JOIN orders AS ord ON c.customer_id = ord.customer_id
 INNER JOIN order_items AS o ON o.order_id = ord.order_id
 GROUP BY c.customer_id
-HAVING count > 1
+HAVING order_count > 1
 ORDER BY total_amount DESC;

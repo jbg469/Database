c2aec2cd2ea74ef1781b553a4248ac0f24cd99e4
jbg469 <72175659+jbg469@users.noreply.github.com>
2022-02-02 13:54:46 -0500

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,4 +1,4 @@
-SET @category_ids = (SELECT ROUP_CONCAT(category_id) FROM categories WHERE category_name='drums' GROUP BY category_name);
+SET @category_ids = (SELECT GROUP_CONCAT(category_id) FROM categories WHERE category_name='drums' GROUP BY category_name);
 UPDATE categories 
 SET category_name = 'Woodwinds'
 WHERE FIND_IN_SET(category_id, @category_ids)

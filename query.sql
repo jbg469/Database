6998a5a395d0ff197b9338d70454283eabb37616
jbg469 <72175659+jbg469@users.noreply.github.com>
2022-02-02 15:50:56 -0500

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,5 +1,5 @@
 SELECT email_address,
-COUNT (orders.order_id) AS order_amount,
+COUNT(orders.order_id) AS order_amount,
 (SUM(item_price)-SUM(discount_amount)*SUM(quantity)) AS order_total
 FROM customers
 JOIN orders
@@ -9,5 +9,5 @@ JOIN order_items
 ON
 orders.order_id= order_iterms.order_id
 GROUP BY email_address
-HAVING COUUT(orders.order_id) >1
+HAVING COUNT(orders.order_id) >1
 ORDER BY SUM(item_price) DESC

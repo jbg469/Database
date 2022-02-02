8206f8a31c30d87027ab015b6573a6032472f669
jbg469 <72175659+jbg469@users.noreply.github.com>
2022-02-02 14:42:23 -0500

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,4 +1,4 @@
-SELECT c.category_name, COUNT(p.category_id) AS product_count, MAX(p.ListPrice) AS most_expensive_product
+SELECT c.category_name, COUNT(p.category_id) AS product_count, MAX(p.list_price) AS most_expensive_product
 FROM categories c, products p
 WHERE p.category_id=c.category_id
 GROUP BY c.category_name ORDER BY COUNT(*) DESC

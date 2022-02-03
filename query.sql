9c29f586bd805d2c0883cf580a988ac34a500d68
jbg469 <72175659+jbg469@users.noreply.github.com>
2022-02-03 13:39:45 -0500

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,8 +1,8 @@
-SELECT  c.email_address, count(o.order_id) AS order_count, sum(quantity*(item_price-discount_amount)) AS order_total
+SELECT  c.email_address, DISTINCT count(o.order_id) AS order_count, sum(quantity*(item_price-discount_amount)) AS order_total
 FROM customers c
     JOIN orders o
         ON c.customer_id = o.customer_id
-    JOIN DISTINCT order_items oi
+    JOIN order_items oi
         ON oi.order_id = o.order_id
 GROUP BY email_address
 HAVING COUNT(o.order_id) > 1

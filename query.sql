8095e52a9bb8f9a26ad7eabf855faa173e0eb692
jbg469 <72175659+jbg469@users.noreply.github.com>
2022-02-11 17:56:42 -0500

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,6 +1,6 @@
-SELECT list_price,
-  CAST(list_price AS decimal(10, 1)) AS list_price_cast,
+SELECT
+    list_price,
+    CAST(list_price AS decimal(10, 1)) AS list_price_cast,
     CONVERT(decimal(10, 1), list_price) AS list_price_convert,
     CAST(list_price AS int) AS list_price_integer
-FROM my_guitar_shop.products;
-ORDER BY list_price ASC;
+FROM my_guitar_shop.products

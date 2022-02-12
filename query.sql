d35effe820e4dc0fe8f48731269e61bf8f0833d6
jbg469 <72175659+jbg469@users.noreply.github.com>
2022-02-11 20:59:25 -0500

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,7 +1,7 @@
 SELECT 
     card_number,
-    LENGTH(o.card_number) AS card_number_length,
-    RIGHT(o.card_number, 4) AS last_four_digits,
+    LENGTH(card_number) AS card_number_length,
+    RIGHT(card_number, 4) AS last_four_digits,
     IF((Length(card_number)-12=4),
     CONCAT('XXXX-XXXX-XXXX-', right(card_number,Length(card_number)-12)),
     CONCAT('XXXX-XXXX-XXXX-X', right(card_number,Length(card_number)-12))) AS formatted_number

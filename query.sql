9438d3291ceb23cee47896f33fb408177e502d6f
jbg469 <72175659+jbg469@users.noreply.github.com>
2022-02-11 21:12:58 -0500

    Update query.sql

--- query.sql
+++ query.sql
@@ -2,9 +2,8 @@ SELECT
     card_number,
     LENGTH(card_number) AS card_number_length,
     RIGHT(card_number, 4) AS last_four_digits,
-    IF LENGHT(card_number)-12=4
-        CONCAT('XXXX-XXXX-XXXX-', right(card_number,LENGTH(card_number)-12)) AS formatted_number
-    ELSE
-        CONCAT('XXXX-XXXX-XXXX-X', right(card_number,LENGHT(card_number)-12))) AS formatted_number
+     IF(LENGTH(card_number) = 16, 
+        CONCAT("XXXX-XXXX-XXXX-", RIGHT (card_number, 4)), 
+        CONCAT("XXXX-XXXXXX-X", RIGHT (card_number, 4))) AS formatted_number
 FROM orders
 ORDER BY card_number ASC;

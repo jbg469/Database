32151f93353db78a77dea3e9c48c8a0640ac02b1
jbg469 <72175659+jbg469@users.noreply.github.com>
2022-02-12 14:18:30 -0500

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,6 +1,6 @@
 CREATE VIEW customer_addresses  AS
 SELECT c.customer_id, email_address, first_name, last_name, 
-city AS bill_city,line2 AS bill_line2, state AS bill_state,line1 AS bill_line1
+city AS bill_city,line2 AS bill_line2, state AS bill_state,line1 AS bill_line1,
 zip_code AS bill_zip, line1 AS ship_line1,line2 AS ship_line2, city AS ship_city, state AS ship_state,
 zip_code AS ship_zip
 FROM customers c 

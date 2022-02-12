d85daf83f74d1c1777214bb60cd7bca1318c11bb
jbg469 <72175659+jbg469@users.noreply.github.com>
2022-02-11 19:37:03 -0500

    Create 8.2.sql

---
+++ 8.2.sql
@@ -0,0 +1,7 @@
+SELECT 
+    date_added,
+    CAST(date_added as date) AS added_date,
+    CAST(date_added AS CHAR (7)) added_char7,
+    CAST(date_added AS TIME) AS added_time
+FROM products;
+ORDER BY date_added ASC

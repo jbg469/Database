920ee0093bc06551179b49ee3fae0c8b9235d4d6
jbg469 <72175659+jbg469@users.noreply.github.com>
2022-02-11 21:43:08 -0500

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,6 +1,5 @@
-SELECT order_id, order_date,
-DATEADD(order_date, INTERVAL 2 DAY) AS approx_ship_date, ship_date, 
-DATEDIFF(ship_date,order_date) AS days_to_ship
+SELECT order_id, order_date, DATE_ADD(order_date, INTERVAL 2 DAY) AS 'approx_ship_date', ship_date, DATEDIFF(ship_date , order_date) AS 'days_to_ship'
 FROM orders
-WHERE month(order_date) = 3
-and YEAR(order_date) = '2018'
+WHERE ship_date IS NOT NULL
+AND MONTH(order_date) = '3'
+AND YEAR(order_date) = '2018';

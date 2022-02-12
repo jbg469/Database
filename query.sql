ab494a8d319f2e1171ea1a4c7632b0f0f64d0c7d
jbg469 <72175659+jbg469@users.noreply.github.com>
2022-02-11 20:52:52 -0500

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,8 +1,12 @@
-SELECT card_number,length(card_number)as card_number,
-right(card_number,4) as last_four_digits,
-if((Length(card_number)-12=4),
-CONCAT('XXXX-XXXX-XXXX-', right(card_number,Length(card_number)-12)),
-CONCAT('XXXX-XXXX-XXXX-X', right(card_number,Length(card_number)-12)))
-AS formatted_number
-FROM orders 
+SELECT 
+    p.product_name,
+    o.card_number,
+    LENGTH(o.card_number) AS card_number_length,
+    RIGHT(o.card_number, 4) AS last_four_digits,
+    IF((Length(card_number)-12=4),
+    CONCAT('XXXX-XXXX-XXXX-', right(card_number,Length(card_number)-12)),
+    CONCAT('XXXX-XXXX-XXXX-X', right(card_number,Length(card_number)-12))) AS formatted_number
+FROM orders o
+JOIN order_items oi ON o.order_id = oi.order_id
+JOIN products p ON oi.product_id = p.product_id
 ORDER BY card_number ASC;

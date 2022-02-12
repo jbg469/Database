105c2fc0c7b1a7dba756ace2960ca7ca8c103d48
jbg469 <72175659+jbg469@users.noreply.github.com>
2022-02-12 14:48:36 -0500

    Create query.sql

---
+++ query.sql
@@ -0,0 +1,5 @@
+CREATE VIEW product_summary AS 
+SELECT COUNT(i.order_id) AS order_count, product_name,
+SUM(((item_price - discount_amount) * quantity)) AS order_total
+FROM orders o JOIN order_items i ON o.order_id = i.order_id
+JOIN products p ON i.product_id = p.product_id

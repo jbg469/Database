5d30ce100a43590f2dd7d0c365435cfd88e99e0a
jbg469 <72175659+jbg469@users.noreply.github.com>
2022-02-12 14:40:37 -0500

    Create query.sql

--- query.sql
+++ query.sql
@@ -1,6 +1,5 @@
 CREATE VIEW order_item_products AS
-SELECT o.order_id, order_date, tax_amount, ship_date, product_name, item_price, discount_amount, 
-(item_price - discount_amount) AS final_price, quantity, ( quantity * (item_price - discount_amount) ) AS item_total 
-FROM orders o
-JOIN order_items i ON o.order_id = i.order_id
+SELECT o.order_id, (quantity * (item_price - discount_amount)) AS item_total, order_id,(item_price - discount_amount) AS final_price, order_date, 
+tax_amount,ship_date, product_name, item_price, discount_amount, quantity 
+FROM orders o JOIN order_items i ON o.order_id = i.order_id
 JOIN products p ON i.product_id = p.product_id

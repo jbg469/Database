b01d76bdd4044b7f219d3f3b1508730f974274ca
jbg469 <72175659+jbg469@users.noreply.github.com>
2022-02-11 22:13:47 -0500

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,2 +1,2 @@
-CREATE INDEX firstname ON customers (first_name),
-CREATE INDEX lastname ON customers (last_name);
+CREATE INDEX firstname ON customers (firstname)
+CREATE INDEX lastname ON customers (lastname);

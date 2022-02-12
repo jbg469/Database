1aa732a5f1a490aa3d94a28e9a897e7cc3f20b06
jbg469 <72175659+jbg469@users.noreply.github.com>
2022-02-12 14:49:39 -0500

    Update query.sql

--- query.sql
+++ query.sql
@@ -3,3 +3,4 @@ SELECT COUNT(i.order_id) AS order_count, product_name,
 SUM(((item_price - discount_amount) * quantity)) AS order_total
 FROM orders o JOIN order_items i ON o.order_id = i.order_id
 JOIN products p ON i.product_id = p.product_id
+GROUP BY product_name

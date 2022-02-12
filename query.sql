50dd60477f86265f8770f69fecabcceb8e300573
jbg469 <72175659+jbg469@users.noreply.github.com>
2022-02-11 20:44:55 -0500

    Update query.sql

--- query.sql
+++ query.sql
@@ -4,11 +4,11 @@ SELECT
     LENGTH(o.card_number) AS card_number_length,
     RIGHT(o.card_number, 4) AS last_four_digits,
     CONCAT('XXXX-XXXX-XXXX-', RIGHT(o.card_number, 4)) as formatted_number,
-FROM
-    orders o
-        JOIN
-    order_items oi ON o.order_id = oi.order_id
-        JOIN
-    products p ON oi.product_id = p.product_id
-ORDER BY p.product_name;
+    IF(LOCATE(' ',SUBSTRING(p.product_name,LOCATE(' ', p.product_name) + 1)) != 0,
+        SUBSTRING_INDEX(SUBSTRING_INDEX(p.product_name, ' ', 3),' ',- 1),
+        '')
+FROM orders o
+JOIN order_items oi ON o.order_id = oi.order_id
+JOIN products p ON oi.product_id = p.product_id
+ORDER BY card_number ASC;
     

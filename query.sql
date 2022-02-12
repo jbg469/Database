f2a5ea11365501ae6e546b372fb40fc5ee8acbd5
jbg469 <72175659+jbg469@users.noreply.github.com>
2022-02-11 20:31:55 -0500

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,7 +1,7 @@
 SELECT 
     order_date,
     DATE_FORMAT(order_date, '%Y') AS order_year,
-    DATE_FORMAT(order_date, '%b-%d-%Y') AS order_date_formatter,
+    DATE_FORMAT(order_date, '%b-%d-%Y') AS order_date_formatted,
     DATE_FORMAT(order_date, '%h:%i %p') AS order_time,
     DATE_FORMAT(order_date, '%m/%d/%y %H:%i') AS order_datetime
 FROM orders

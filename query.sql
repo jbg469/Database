c89524a3df39ecca99a0bf65e0f26349512484ab
jbg469 <72175659+jbg469@users.noreply.github.com>
2022-02-11 21:36:53 -0500

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,5 +1,5 @@
 SELECT order_id, order_date,
-DATEADD(Order_date, INTERVAL 2 DAY) AS approx_ship_date, ship_date, 
+DATEADD(order_date, INTERVAL 2 DAY) AS approx_ship_date, ship_date, 
 DATEDIFF(ship_date,order_date) AS days_to_ship
 FROM orders
 WHERE month(order_date) = 3

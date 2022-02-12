9fb37bb74d199a7db6a80bf183037dde4106b626
jbg469 <72175659+jbg469@users.noreply.github.com>
2022-02-11 20:58:16 -0500

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,10 +1,9 @@
 SELECT 
-    p.product_name,
-    o.card_number,
+    card_number,
     LENGTH(o.card_number) AS card_number_length,
     RIGHT(o.card_number, 4) AS last_four_digits,
     IF((Length(card_number)-12=4),
     CONCAT('XXXX-XXXX-XXXX-', right(card_number,Length(card_number)-12)),
     CONCAT('XXXX-XXXX-XXXX-X', right(card_number,Length(card_number)-12))) AS formatted_number
-FROM orders o
+FROM orders
 ORDER BY card_number ASC;

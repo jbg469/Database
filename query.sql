bbdb6149ac39d8e8e3d8a021ea1bb83a216503b1
jbg469 <72175659+jbg469@users.noreply.github.com>
2022-03-30 22:06:36 -0400

    Update and rename 13.3 to query.sql

--- query.sql
+++ query.sql
@@ -2,11 +2,9 @@ CREATE PROCEDURE test(
 OUT message VARCHAR(60))
 BEGIN
 DECLARE productCount DECIMAL DEFAULT 0;
-
 SELECT COUNT(*)
 INTO productCount
 FROM products;
-
 IF productCount >= 7 THEN
 SET message = 'The number of products is greater than or equal to 7';
 ELSE

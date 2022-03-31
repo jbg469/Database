f7e974a30818a10186378cf68ee76cc5cf3913d7
jbg469 <72175659+jbg469@users.noreply.github.com>
2022-03-30 23:01:28 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,5 +1,10 @@
-CREATE PROCEDURE insert_category(IN result VARCHAR(400))
+CREATE FUNCTION discount_price(item_id INT)
+returns DECIMAL(10,2)
 BEGIN
- INSERT INTO categories (category_name)
- VALUES (result);
+  DECLARE discount_price DECIMAL(10,2);
+  SELECT (oi.item_price - oi.discount_amount) INTO discount_price
+  FROM order_items AS oi
+  where oi.item_id = item_id;
+  return discount_price;
+
 END

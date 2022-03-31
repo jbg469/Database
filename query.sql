8761645bb86aed2d9a526a09b98c960085fb07f1
jbg469 <72175659+jbg469@users.noreply.github.com>
2022-03-30 22:11:42 -0400

    Create query.sql

---
+++ query.sql
@@ -0,0 +1,21 @@
+CREATE PROCEDURE test(OUT result VARCHAR(400))
+BEGIN
+ DECLARE product_name_var VARCHAR(50);
+ DECLARE list_price_var DECIMAL(9,2);
+ DECLARE row_not_found TINYINT DEFAULT FALSE;
+ DECLARE s_var VARCHAR(400) DEFAULT '';
+ DECLARE invoice_cursor CURSOR for
+ SELECT product_name, list_price
+ FROM products
+ WHERE list_price > 700
+ ORDER BY list_price DESC;  
+ DECLARE CONTINUE HANDLER FOR NOT FOUND
+ SET row_not_found = TRUE;
+ OPEN invoice_cursor;
+ FETCH invoice_cursor INTO product_name_var, list_price_var;
+ WHILE row_not_found = FALSE DO
+     SET s_var = CONCAT(s_var,'*', product_name_var,'*,*',list_price_var,'*|');
+  FETCH invoice_cursor INTO product_name_var, list_price_var;
+ END WHILE;
+ SELECT s_var INTO result;
+END

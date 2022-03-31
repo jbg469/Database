dc6c784165d7178a23dd346546016af50f8e9b0d
jbg469 <72175659+jbg469@users.noreply.github.com>
2022-03-30 21:49:13 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -5,7 +5,7 @@ BEGIN
   DECLARE number_2_var   INT DEFAULT 20;
   DECLARE message_var    VARCHAR(400);
   SET message_var = CONCAT('Common factors of 10 and 20:');
-  WHILE ((i <= number_1_var))DO
+  WHILE ((b <= number_1_var))DO
     IF ((number_1_var % b = 0) AND (number_2_var % b = 0)) THEN
       SET message_var = CONCAT(message_var," ", b);
       END IF;

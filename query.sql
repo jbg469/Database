5415e9165976e9942333633e8077f424b210b165
jbg469 <72175659+jbg469@users.noreply.github.com>
2022-04-03 17:20:16 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,10 +1,25 @@
-CREATE FUNCTION discount_price(item_id INT)
-returns DECIMAL(10,2)
+CREATE PROCEDURE test()
 BEGIN
-  DECLARE discount_price DECIMAL(10,2);
-  SELECT (oi.item_price - oi.discount_amount) INTO discount_price
-  FROM order_items AS oi
-  where oi.item_id = item_id;
-  return discount_price;
+ DECLARE product_name_var VARCHAR(50);
+ DECLARE list_price_var DECIMAL(9,2);
+ DECLARE row_not_found TINYINT DEFAULT FALSE;
+ DECLARE s_var VARCHAR(400) DEFAULT '';
+ DECLARE invoice_cursor CURSOR for
+ SELECT product_name, list_price
+ FROM products
+ WHERE list_price > 700
+ ORDER BY list_price DESC;
+    
+ DECLARE CONTINUE HANDLER FOR NOT FOUND
+ SET row_not_found = TRUE;
 
+ OPEN invoice_cursor;
+
+ FETCH invoice_cursor INTO product_name_var, list_price_var;
+ WHILE row_not_found = FALSE DO
+     SET s_var = CONCAT(s_var,'*', product_name_var,'*,*',list_price_var,'*|');
+  FETCH invoice_cursor INTO product_name_var, list_price_var;
+ END WHILE;
+
+ SELECT s_var AS result;
 END
